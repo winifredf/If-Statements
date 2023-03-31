31bd10749b8bca9b9b862a1669299c8c54934b5d
Winifredf <Winifred.Dykes@hotmail.com>
2023-03-31 00:20:40 -0400

    added comment output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 is_happy = True
 and_you_know_it = True 
-if is_happy:
+if is_happy or and_you_know_it:
     print("I am fulfilled")
 else:
-    print("I am not fulfilled")
+    print("I am not fulfilled")\
+# Output: I am fulfilled
