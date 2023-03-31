d2d97770c781b5ae071301846cd5f3481ded973b
Winifredf <Winifred.Dykes@hotmail.com>
2023-03-31 00:21:59 -0400

    removed backslash

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ and_you_know_it = True
 if is_happy or and_you_know_it:
     print("I am fulfilled")
 else:
-    print("I am not fulfilled")\
+    print("I am not fulfilled")
 # Output: I am fulfilled
