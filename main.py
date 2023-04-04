49eb33cbbbfdc4e90871fa61780de2cddf7ed391
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-03 21:47:22 -0400

    set is_tired variable to False used in last stmt

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ else:
 on_vacation = True
 go_to_beach = True
 is_hungry = True
+if_tired = False
 if on_vacation and go_to_beach:
     print("I wear my swimsuit")
 elif on_vacation and not go_to_beach:
     print("I wear my casual clothes")
 elif on_vacation and is_hungry:
     print("I go to the restaurant")
+else on_vacation and if_tired:
+    print("I go to sleep")
